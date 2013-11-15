d1bd18bc8bf7423da4c69c7de0ecec0633aaf3ab
Jonathan Reed <jdreed@mit.edu>
2013-11-15 16:14:12 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='locker-support',
+      version='1.0',
+      author='Debathena Project',
+      author_email='debathena@mit.edu',
+      py_modules=['locker', 'athdir'])
