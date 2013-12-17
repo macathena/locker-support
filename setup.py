939f7a3ff98fe8217089c031500c4e4d954e5508
Jonathan Reed <jdreed@mit.edu>
2013-12-17 16:10:03 -0500

    Add packaging for scripts
    - Add the Python scripts to setup.py
    - Override dh_auto_install to install the scripts to usr/lib
    - Create .install files for both packages to correctly install
      the modules and userland scripts.

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name='locker-support',
       version='1.0',
       author='Debathena Project',
       author_email='debathena@mit.edu',
-      py_modules=['locker', 'athdir'])
+      py_modules=['locker', 'athdir'],
+      scripts=['attach', 'detach', 'fsid', 'quota', 'athdir'],
+      )
